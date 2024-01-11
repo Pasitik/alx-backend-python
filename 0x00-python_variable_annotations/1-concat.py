272c972d42c85716189781d5d39f85e97729c80d
Pasitik <paasitik@gmail.com>
2024-01-11 21:28:14 +0000

    typings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 2's module.
+'''
+
+
+def concat (str1: str, str2: str)-> str:
+    '''Computes the floor of a floating-point number.
+    '''
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+type checking in python
