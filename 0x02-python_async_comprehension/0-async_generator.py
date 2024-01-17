2a9a72001cf4b0ddf7f6ddd62e1a95d776c747e0
Pasitik <paasitik@gmail.com>
2024-01-17 00:49:23 +0000

    .\0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
+import asyncio
+import random
+
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
