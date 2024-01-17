1c9e633bec68199cc602262b18ed310cb9ab0009
Pasitik <paasitik@gmail.com>
2024-01-17 02:34:04 +0000

    .\1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,13 @@
 '''Task 1's module.
 '''
 from typing import List
+from importlib import import_module as using
 
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     '''Creates a list of 10 numbers from a 10-number generator.
-    '''    
-    return [n async for n in async_generator()]
+    '''
+    return [i async for i in async_generator()]
